e099f82332c7f332238e93add64eec4bf817b0d7
marco <marco@MARCO-LAPTOP>
2012-01-30 00:02:10 +0100

    primer caso con ? y ifs

--- GroovyBenchmarks/src/test/NullChecking.groovy
+++ GroovyBenchmarks/src/test/NullChecking.groovy
@@ -4,5 +4,29 @@ class NullChecking {
 
 	public static void main(String[] args) {
 		//check ? operator 
+		int times = 100000
+		def a = null
+		
+		def b
+		
+		
+		long t1 = new Date().getTime()
+		for (int i = 0; i < times; i++) {
+			b = a?.class
+		}
+		long t1f = new Date().getTime() - t1
+		println "Operator ?: \t " + t1f + "ms"
+		
+		long t2 = new Date().getTime()
+		for (int i = 0; i < times; i++) {
+			if(a!=null){
+				b=a.class
+			}else{
+				b=null
+			}
+		
+		}
+		long t2f = new Date().getTime() - t2
+		println "If clause:\t " + t2f + "ms"
 	}
 }
